c623522220763c3408ced26769cd06b989a3cb12
Joel Kanzelmeyer <joelk@purecars.com>
2015-06-04 03:05:02 -0400

    Created video-bg directive

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+/*jslint node: true */
+'use strict';
+
+var pkg = require('./package.json');
+
+module.exports = function (grunt) {
+
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      main: {
+        options: {
+          port: 9001
+        }
+      }
+    },
+    watch: {
+      main: {
+        options: {
+            livereload: true,
+            livereloadOnError: false,
+            spawn: false
+        },
+        files: ['src/**/*.js'],
+        tasks: [] //all the tasks are run dynamically during the watch event handler
+      }
+    },
+    jshint: {
+      main: {
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        src: ['src/**/*.js']
+      }
+    },
+    clean: {
+      before:{
+        src:['dist']
+      }
+    },
+    concat: {
+      main: {
+        src: ['src/angular-video-bg.js'],
+        dest: 'dist/angular-video-bg.js'
+      }
+    },
+    ngAnnotate: {
+      main: {
+        src:'dist/angular-video-bg.js',
+        dest: 'dist/angular-video-bg.js'
+      }
+    },
+    uglify: {
+      main: {
+        src: 'dist/angular-video-bg.js',
+        dest:'dist/angular-video-bg.min.js'
+      }
+    },
+    karma: {
+      options: {
+        frameworks: ['jasmine'],
+        files: [  //this files data is also updated in the watch handler, if updated change there too
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+          'src/**/*.js'
+        ],
+        logLevel:'ERROR',
+        reporters:['mocha'],
+        autoWatch: false, //watching is handled by grunt-contrib-watch
+        singleRun: true
+      },
+      all_tests: {
+        browsers: ['PhantomJS','Chrome','Firefox']
+      },
+      during_watch: {
+        browsers: ['PhantomJS']
+      }
+    }
+  });
+
+  grunt.registerTask('build',['jshint','clean:before','concat','ngAnnotate','uglify']);
+  grunt.registerTask('serve', ['jshint','connect', 'watch']);
+  grunt.registerTask('test',['karma:all_tests']);
+
+  grunt.event.on('watch', function(action, filepath) {
+    //https://github.com/gruntjs/grunt-contrib-watch/issues/156
+
+    var tasksToRun = [];
+
+    if (filepath.lastIndexOf('.js') !== -1 && filepath.lastIndexOf('.js') === filepath.length - 3) {
+
+      //lint the changed js file
+      grunt.config('jshint.main.src', filepath);
+      tasksToRun.push('jshint');
+
+      //find the appropriate unit test for the changed file
+      var spec = filepath;
+      if (filepath.lastIndexOf('-spec.js') === -1 || filepath.lastIndexOf('-spec.js') !== filepath.length - 8) {
+        spec = filepath.substring(0,filepath.length - 3) + '-spec.js';
+      }
+
+      //if the spec exists then lets run it
+      if (grunt.file.exists(spec)) {
+        tasksToRun.push('karma:during_watch');
+      }
+    }
+
+    grunt.config('watch.main.tasks',tasksToRun);
+
+  });
+};
---
+++ src/angular-video-bg-spec.js
@@ -0,0 +1,56 @@
+describe('video-bg directive', function() {
+
+    beforeEach(module('angularVideoBg'));
+
+    var scope, compile, timeout, videoBg, videoBgScope, $player, $content;
+
+    beforeEach(inject(function($rootScope, $compile, $timeout) {
+        scope = $rootScope.$new();
+        compile = $compile;
+        timeout = $timeout;
+
+        videoBg = '<video-bg video-id="videoId" ratio="ratio" content-z-index="zIndex"><p>test</p></video-bg>';
+        scope.videoId = 'M7lc1UVf-VE';
+        videoBg = compile(videoBg)(scope);
+        scope.$digest();
+
+        $player = videoBg.children().eq(0);
+        $content = videoBg.children().eq(1);
+        videoBgScope = videoBg.isolateScope();
+    }));
+
+    it('should create a video player element', function() {
+        expect($player.attr('id')).toBe('player');
+    });
+
+    it('should have correct video id', function() {
+        expect(videoBgScope.videoId).toBe('M7lc1UVf-VE');
+    });
+
+    it('should create a content element', function() {
+        expect($content).toBeDefined();
+    });
+
+    describe('content element', function() {
+
+        it('should content the transcluded content', function() {
+            expect($content.find('p').text()).toBe('test');
+        });
+
+    });
+
+    describe('aspect ratio', function() {
+
+        it('should be 16/9 by default', function() {
+            expect(videoBgScope.ratio).toBe(16/9);
+        });
+
+        it('should be changed if ratio attribute is added', function() {
+            scope.ratio = 4/3;
+            scope.$digest();
+            expect(videoBgScope.ratio).toBe(4/3);
+        });
+
+    });
+
+});
