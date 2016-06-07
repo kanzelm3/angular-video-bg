bf9443075ecbd60c4dd0e73ce12c8ec77502a746
Shannon Hochkins <shannon.hochkins@havasww.com>
2016-06-08 09:36:46 +1000

    Exposing internal methods for each player-callback option, adding functionality so that each directive can pass in different player variables as a scope parameter

--- angular-video-bg.js
+++ angular-video-bg.js
@@ -38,6 +38,7 @@
                 contentZIndex: '=?',
                 allowClickEvents: '=?',
                 mobileImage: '=?',
+                playerVars : '=?',
                 playerCallback: '&?'
             },
             transclude: true,
@@ -353,7 +354,12 @@
                 function playerReady() {
                     if (playerCallback) {
                         $timeout(function() {
-                            playerCallback({ player: player });
+                            playerCallback({ 
+                                player: player, 
+                                methods : {
+                                    resize : resizeAndPositionPlayer
+                                } 
+                            });
                         });
                     }
                     if (scope.playlist) {
@@ -428,6 +434,9 @@
                         showinfo: 0,
                         playlist: scope.videoId
                     };
+                    if (angular.isObject(scope.playerVars)) {
+                        playerOptions = angular.extend({}, playerOptions, scope.playerVars);
+                    }
                     player = new YT.Player(playerId, {
                         width: playerDimensions.width,
                         height: playerDimensions.height,
@@ -506,6 +515,10 @@
                         $player.css('display', 'none');
                         player.loadVideoById(current);
                     }
+                });
+
+                scope.$on('video-bg:resize', function() {
+
                 });
 
                 scope.$watchCollection('playlist', function(current, old) {
